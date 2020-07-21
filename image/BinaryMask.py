038b34229192b4f196cd719927345fb71295af5c
Eytan Kats <eytan.kats@gmail.com>
2020-07-21 21:21:59 +0300

    remove 'resize' method from 'BinaryMask' class to prevent code duplication

--- image/BinaryMask.py
+++ image/BinaryMask.py
@@ -6,19 +6,8 @@ from image.Image import Image
 class BinaryMask(Image):
 
     """
-    This class defines methods specific for binary masks
+    This class defines methods specific for single channel binary masks
     """
-
-    def resize(self, shape, interpolation=cv2.INTER_NEAREST):
-
-        """
-        This method resizes mask
-        :param shape: new shape of the mask: (width, height)
-        :param interpolation: interpolation method (default: nearest)
-        :return: None
-        """
-
-        cv2.resize(self.pixel_data, shape, interpolation)
 
     def remove_holes(self):
 
