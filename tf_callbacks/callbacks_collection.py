1c88bf65c282af028015a289ceedf2cdf1ace43b
Eytan Kats <eytan.kats@gmail.com>
2020-08-10 09:38:37 +0300

    add tensorboard callback to callbacks collection

--- tf_callbacks/callbacks_collection.py
+++ tf_callbacks/callbacks_collection.py
@@ -2,11 +2,13 @@ from tf_callbacks.CheckpointCallback import CheckpointCallback
 from tf_callbacks.CsvLoggerCallback import CsvLoggerCallback
 from tf_callbacks.EarlyStoppingCallback import EarlyStoppingCallback
 from tf_callbacks.ReduceLrOnPlateauCallback import ReduceLrOnPlateauCallback
+from tf_callbacks.TensorBoardCallback import TensorBoardCallback
 
 callbacks_collection = {
 
     "checkpoint_callback": CheckpointCallback,
     "csv_logger_callback": CsvLoggerCallback,
     "early_stopping_callback": EarlyStoppingCallback,
-    "reduce_lr_on_plateau": ReduceLrOnPlateauCallback
+    "reduce_lr_on_plateau": ReduceLrOnPlateauCallback,
+    "tensorboard": TensorBoardCallback
 }
