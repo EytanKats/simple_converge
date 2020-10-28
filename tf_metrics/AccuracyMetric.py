ac69acb662dc81fe9d00f446ceed37eb7665418c
Eytan Kats <eytan.kats@gmail.com>
2020-10-28 09:18:57 +0200

    enhancement: binary accuracy metric

--- tf_metrics/AccuracyMetric.py
+++ tf_metrics/AccuracyMetric.py
@@ -18,6 +18,7 @@ class AccuracyMetric(BaseMetric):
 
         super(AccuracyMetric, self).__init__()
 
+        self.binary_accuracy = False
         self.categorical_accuracy = False
 
     def parse_args(self, **kwargs):
@@ -30,16 +31,29 @@ class AccuracyMetric(BaseMetric):
 
         super(AccuracyMetric, self).parse_args(**kwargs)
 
+        if "binary_accuracy" in self.params.keys():
+            self.binary_accuracy = self.params["binary_accuracy"]
+
         if "categorical_accuracy" in self.params.keys():
             self.categorical_accuracy = self.params["categorical_accuracy"]
 
     def get_metric(self):
+
+        if self.binary_accuracy:
+            return self.binary_accuracy_metric()
 
         if self.categorical_accuracy:
             return self.categorical_accuracy_metric()
 
         else:
             raise ValueError("The metric is not found")
+
+    @staticmethod
+    def binary_accuracy_metric():
+
+        accuracy = tf.keras.metrics.BinaryAccuracy()
+
+        return accuracy
 
     @staticmethod
     def categorical_accuracy_metric():
