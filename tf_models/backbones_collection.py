c76ca59eeae0312c8061a5dabf66ae624597d621
Eytan Kats <eytan.kats@gmail.com>
2020-12-28 10:21:21 +0200

    update 'backbones_collection' and 'models_collection' according to new folders hierarchy

--- tf_models/backbones_collection.py
+++ tf_models/backbones_collection.py
@@ -1,11 +1,11 @@
-from tf_models.ResNet50 import ResNet50
-from tf_models.ResNet101 import ResNet101
-from tf_models.ResNet152 import ResNet152
-from tf_models.ResNet50V2 import ResNet50V2
-from tf_models.DenseNet121 import DenseNet121
-from tf_models.EfficientNetB5 import EfficientNetB5
-from tf_models.EfficientNetB6 import EfficientNetB6
-from tf_models.EfficientNetB7 import EfficientNetB7
+from tf_models.third_party_models.ResNet50 import ResNet50
+from tf_models.third_party_models.ResNet101 import ResNet101
+from tf_models.third_party_models.ResNet152 import ResNet152
+from tf_models.third_party_models.ResNet50V2 import ResNet50V2
+from tf_models.third_party_models.DenseNet121 import DenseNet121
+from tf_models.third_party_models.EfficientNetB5 import EfficientNetB5
+from tf_models.third_party_models.EfficientNetB6 import EfficientNetB6
+from tf_models.third_party_models.EfficientNetB7 import EfficientNetB7
 
 backbones_collection = {
 
