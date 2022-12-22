7b55d19eda173cc42c6c556252438cf0083b0543
Eytan Kats <eytan.kats@gmail.com>
2022-12-22 12:51:32 +0200

    improvement: separate encoder and projector in 'MomentumApp'

--- apps/MomentumApp.py
+++ apps/MomentumApp.py
@@ -39,23 +39,33 @@ class MomentumApp(BaseApp):
             metric,
         )
 
+        # Create models' architecture
         if architecture is not None:
-            self.base_encoder = architecture['base_encoder'](settings)
-            self.momentum_encoder = architecture['base_encoder'](settings)
+            self.base_encoder = architecture['encoder'](settings)
+            self.momentum_encoder = architecture['encoder'](settings)
+            self.base_projector = architecture['projector'](settings)
+            self.momentum_projector = architecture['projector'](settings)
             self.predictor = architecture['predictor'](settings)
         else:
             self.base_encoder = None
             self.momentum_encoder = None
+            self.base_projector = None
+            self.momentum_projector = None
             self.predictor = None
 
-        # Initialize parameters of momentum encoder
+        # Initialize parameters of momentum encoder to be equal to parameters of base encoder
         for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
+            param_m.data.copy_(param_b.data)  # initialize
+            param_m.requires_grad = False  # prevent update by gradient
+
+        # Initialize parameters of momentum projector to be equal to parameters of base projector
+        for param_b, param_m in zip(self.base_projector.parameters(), self.momentum_projector.parameters()):
             param_m.data.copy_(param_b.data)  # initialize
             param_m.requires_grad = False  # prevent update by gradient
 
         if optimizer is not None:
             # Instantiate optimizer for base encoder and predictor but not for the momentum encoder
-            self.optimizer = optimizer(settings, self.base_encoder, self.predictor)
+            self.optimizer = optimizer(settings, self.base_encoder, self.base_projector, self.predictor)
         else:
             self.optimizer = None
 
@@ -79,6 +89,8 @@ class MomentumApp(BaseApp):
         checkpoint = torch.load(ckpt_path)
         self.base_encoder.load_state_dict(checkpoint['base_encoder'])
         self.momentum_encoder.load_state_dict(checkpoint['momentum_encoder'])
+        self.base_projector.load_state_dict(checkpoint['base_projector'])
+        self.momentum_projector.load_state_dict(checkpoint['momentum_projector'])
         self.predictor.load_state_dict(checkpoint['predictor'])
 
         if self.optimizer is not None:
@@ -92,6 +104,8 @@ class MomentumApp(BaseApp):
         torch.save({
             'base_encoder': self.base_encoder.state_dict(),
             'momentum_encoder': self.momentum_encoder.state_dict(),
+            'base_projector': self.base_projector.state_dict(),
+            'momentum_projector': self.momentum_projector.state_dict(),
             'predictor': self.predictor.state_dict(),
             'optimizer': self.optimizer.state_dict()
         },
@@ -111,16 +125,23 @@ class MomentumApp(BaseApp):
             self.scheduler.step()
 
     @torch.no_grad()
-    def _update_momentum_encoder(self, m):
+    def _update_momentum_encoder_and_projector(self, m):
 
         # Update parameters of momentum encoder
         for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
             param_m.data = param_m.data * m + param_b.data * (1. - m)
 
+        # Update parameters of momentum projector
+        for param_b, param_m in zip(self.base_projector.parameters(), self.momentum_projector.parameters()):
+            param_m.data = param_m.data * m + param_b.data * (1. - m)
+
     def training_step(self, data, epoch):
 
         self.base_encoder.train()
+        self.base_projector.train()
+        self.predictor.train()
 
+        # Get two views (different augmentations) of the data
         view_1 = data[0].to(self.device)
         view_2 = data[1].to(self.device)
 
@@ -130,30 +151,38 @@ class MomentumApp(BaseApp):
         base_features_1 = self.base_encoder(view_1)
         base_features_2 = self.base_encoder(view_2)
 
+        # Compute projections of base projector
+        base_projections_1 = self.base_projector(base_features_1)
+        base_projections_2 = self.base_projector(base_features_2)
+
         # Compute predictions
-        predictions_1 = self.predictor(base_features_1)
-        predictions_2 = self.predictor(base_features_2)
+        predictions_1 = self.predictor(base_projections_1)
+        predictions_2 = self.predictor(base_projections_2)
 
         with torch.no_grad():  # disable gradient calculation
-            self._update_momentum_encoder(self.ema_decay)  # update the momentum encoder
+            self._update_momentum_encoder_and_projector(self.ema_decay)  # update the momentum encoder
 
             # Compute features of momentum encoder
             momentum_features_1 = self.momentum_encoder(view_1)
             momentum_features_2 = self.momentum_encoder(view_2)
 
+            # Compute features of momentum projector
+            momentum_projections_1 = self.momentum_projector(momentum_features_1)
+            momentum_projections_2 = self.momentum_projector(momentum_features_2)
+
         # Calculate loss
         batch_loss_list = list()
 
-        loss = self.losses_fns[0](predictions_1, momentum_features_2)\
-               + self.losses_fns[0](predictions_2, momentum_features_1)
+        loss = self.losses_fns[0](predictions_1, momentum_projections_2)\
+            + self.losses_fns[0](predictions_2, momentum_projections_1)
         batch_loss_list.append(loss.detach().cpu().numpy())
 
         # Calculate metrics
         batch_metric_list = list()
 
         for metric_fn in self.metrics_fns:
-            metric = metric_fn(predictions_1, momentum_features_2)\
-                     + metric_fn(predictions_2, momentum_features_1)
+            metric = metric_fn(predictions_1, momentum_projections_2)\
+                + metric_fn(predictions_2, momentum_projections_1)
             batch_metric_list.append(metric.detach().cpu().numpy())
 
         # Backward pass
@@ -165,37 +194,46 @@ class MomentumApp(BaseApp):
     def validation_step(self, data, epoch):
 
         self.base_encoder.eval()
+        self.base_projector.eval()
+        self.predictor.eval()
 
         view_1 = data[0].to(self.device)
         view_2 = data[1].to(self.device)
 
         with torch.no_grad():
-
             # Compute features of base encoder
             base_features_1 = self.base_encoder(view_1)
             base_features_2 = self.base_encoder(view_2)
 
+            # Compute projections of base projector
+            base_projections_1 = self.base_projector(base_features_1)
+            base_projections_2 = self.base_projector(base_features_2)
+
             # Compute predictions
-            predictions_1 = self.predictor(base_features_1)
-            predictions_2 = self.predictor(base_features_2)
+            predictions_1 = self.predictor(base_projections_1)
+            predictions_2 = self.predictor(base_projections_2)
 
             # Compute features of momentum encoder
             momentum_features_1 = self.momentum_encoder(view_1)
             momentum_features_2 = self.momentum_encoder(view_2)
 
+            # Compute features of momentum projector
+            momentum_projections_1 = self.momentum_projector(momentum_features_1)
+            momentum_projections_2 = self.momentum_projector(momentum_features_2)
+
             # Calculate loss
             batch_loss_list = list()
 
-            loss = self.losses_fns[0](predictions_1, momentum_features_2) \
-                   + self.losses_fns[0](predictions_2, momentum_features_1)
+            loss = self.losses_fns[0](predictions_1, momentum_projections_2) \
+                + self.losses_fns[0](predictions_2, momentum_projections_1)
             batch_loss_list.append(loss.detach().cpu().numpy())
 
             # Calculate metrics
             batch_metric_list = list()
 
             for metric_fn in self.metrics_fns:
-                metric = metric_fn(predictions_1, momentum_features_2) \
-                         + metric_fn(predictions_2, momentum_features_1)
+                metric = metric_fn(predictions_1, momentum_projections_2) \
+                    + metric_fn(predictions_2, momentum_projections_1)
                 batch_metric_list.append(metric.detach().cpu().numpy())
 
         return batch_loss_list, batch_metric_list
@@ -222,8 +260,14 @@ class MomentumApp(BaseApp):
         logger.info("Base encoder architecture:")
         print(self.base_encoder)
 
+        logger.info("Base projector architecture:")
+        print(self.base_projector)
+
         logger.info("Momentum encoder architecture:")
         print(self.momentum_encoder)
+
+        logger.info("Momentum projector architecture:")
+        print(self.momentum_projector)
 
         logger.info("Predictor architecture:")
         print(self.predictor)
